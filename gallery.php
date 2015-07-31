8abe3c4a1f1a7bb9d7c126b65ca6d303d323a07e
Richard Perry <richard@perrymail.me.uk>
2015-07-31 13:11:30 +0100

    Testing nanoGallery

---
+++ gallery.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width initial-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='robots' content='noindex, nofollow'>
+
+    <title>Home | Richard Perry</title>
+    <meta name='author' content='Richard Perry'>
+    <meta name='description' content='This is my own personal site for keeping a basic journal and some notes for my Chinese Language Learning. Hopefully, once my Chinese improved, I will also start writing some journal entries in Mandarin. The site is not for general consumption, but I like Jekyll   & GitHub so I am using the combination for my own benefit.
+'>
+
+    <link rel='stylesheet' href='//fonts.googleapis.com/css?family=Source+Sans+Pro:400italic,600italic,400,600|Indie+Flower&subset=latin,latin-ext' type='text/css'>
+    <link rel='stylesheet' href='//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'>
+    <link rel='stylesheet' href='//yui.yahooapis.com/pure/0.5.0/pure-min.css'>
+    <link rel="stylesheet" href="//yui.yahooapis.com/pure/0.5.0/grids-responsive-min.css">
+    <link rel="stylesheet" href="http://zh.perry-online.me.uk/css/blog.css">
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.7.0/css/nanogallery.min.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/nanogallery/5.7.0/jquery.nanogallery.min.js"></script>
+    
+    <!--[if lt IE 9]>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"> </script>
+    <![endif]-->
+    
+    <link rel='canonical' href='http://zh.perry-online.me.uk/'>
+</head>
+    
+<body>
+    <div id='container' class='pure-g'>
+        
+        <header class='site-header pure-u-1 pure-u-md-1-4'>
+            <object type='image/svg+xml' data='http://zh.perry-online.me.uk/images/logo.svg' title='Richard Perry' name='logo'>Your browser does not support SVG</object>
+            <h1 class='site-title'><a href='http://zh.perry-online.me.uk/'>Richard Perry</a></h1>
+            <nav class='site-navigation pure-menu pure-menu-open'>
+                <ul> 
+                    <li><a class='pure-menu-selected' href='/'>Home</a></li> 
+                    <li><a  href='/en/'>English</a></li> 
+                    <li><a  href='/zh/'>中文 (Chinese)</a></li> 
+                    <li><a  href='/tags/'>Tags</a></li>  
+                    <li><a  href='/archives/'>Archives</a></li> 
+                </ul>
+            </nav>
+            <p class="site-description">This is my own personal site for keeping a basic journal and some notes for my Chinese Language Learning. Hopefully, once my Chinese improved, I will also start writing some journal entries in Mandarin. The site is not for general consumption, but I like Jekyll   & GitHub so I am using the combination for my own benefit.
+</p>
+        </header>
+
+
+        <main class="site-content pure-u-1 pure-u-md-3-4">
+                        <h2 class="page-title">Gallery</h2>
+                        <div id="nanoGallery1"></div>
+                        
+                        <script>
+                          $(document).ready(function () {
+                              $("#nanoGallery1").nanoGallery({
+                                  kind: 'flickr',
+                                  userID: '8676505@N02'
+                              });
+                          });
+                        </script>
+        </main>
+
+        <footer class="site-footer pure-u-1 pure-u-md-3-4">
+            <ul class='footer-navigation pure-menu pure-menu-open pure-menu-horizontal'>
+                <li><a href='http://zh.perry-online.me.uk/' title='Richard Perry'>Home</a></li>
+            </ul>
+        </footer>
+    </div> 
+</body>
+</html>
